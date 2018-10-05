1f61ed9b4764dade54f145b88fa283c67df036f7
MerceyAnderson <43886391+MerceyAnderson@users.noreply.github.com>
2018-10-05 22:14:25 +0300

    Create require_input_and_triple_greeting.rb

---
+++ require_input_and_triple_greeting.rb
@@ -0,0 +1,18 @@
+# Приложение,которое требует ввода от пользователя и приветствует трижды.
+
+print "What's your name? "
+name = gets.chomp
+name.capitalize!
+
+puts # Не понял как абзацы делать(
+
+print "You're password : "
+password = gets.chomp
+
+puts # Не понял как абзацы делать( (x2)
+
+puts  "Authorization completed"
+
+puts # Не понял как абзацы делать( (x3)
+
+puts  "Welcome #{name}. Hello! Hello! Hello!"
